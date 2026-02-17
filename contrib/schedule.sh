708b4ca679ab8a1c30dcf87f64a64752d231c3dc
francesco <info.francescobianco@gmail.com>
2026-02-17 22:08:43 +0100

    release

--- contrib/schedule.sh
+++ contrib/schedule.sh
@@ -4,15 +4,18 @@ set -e
 if [ -z "$2" ]; then
   echo "Schedule task: $1"
 
+  if [ "$1" -gt "0" ]; then
+    echo "Reset timer for task: $1"
+    docker compose run --rm mysql sh -c 'mysql -h0.0.0.0 -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" -e "UPDATE vtiger_cron_task SET laststart = 0, lastend = 0, status = 1 WHERE id = '"$1"';"' || true
   exit 0
 fi
 
 echo "Schedule: $2"
 
-variables=$(grep "^crm=$1 " .hosts | head -1)
+variables=$(grep "^crm=$2 " .hosts | head -1)
 
 if [ -z "$variables" ]; then
-    echo "No such host: $1"
+    echo "No such host: $2"
     exit 1
 fi
 
