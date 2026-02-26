0ccd8e70686446e9ff8facd3eb0b10ae57b49814
francesco <info.francescobianco@gmail.com>
2026-02-26 18:08:50 +0100

    release

--- contrib/apply.sh
+++ contrib/apply.sh
@@ -40,18 +40,19 @@ if [ ! -f .env ]; then
   cp .env.examples .env
 fi
 
+## Prepare .env
 cp .env.examples .env
 sed -i "s/CRM_HOST=.*/CRM_HOST=$crm_host/g" .env
 sed -i "s/GOOGLE_CLIENT_ID=.*/GOOGLE_CLIENT_ID=$google_client_id/g" .env
 sed -i "s/GOOGLE_CLIENT_SECRET=.*/GOOGLE_CLIENT_SECRET=$google_client_secret/g" .env
 touch lib/config.inc.php
 
+## Prepare Google connector
+cp lib/modules/Google/connectors/Config.php.examples lib/modules/Google/connectors/Config.php
 sed -i \
   -e "s|static \$clientId = ''|static \$clientId = '${google_client_id}'|" \
   -e "s|static \$clientSecret = ''|static \$clientSecret = '${google_client_secret}'|" \
   lib/modules/Google/connectors/Config.php
-
-cat lib/modules/Google/connectors/Config.php
 
 echo "==> Restart services"
 make apply
