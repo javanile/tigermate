b39483780c84ccf0b0c883c381ce0dab88ec8a42
francesco <info.francescobianco@gmail.com>
2026-02-26 16:41:06 +0100

    release

--- lib/modules/Google/handlers/Cron.php
+++ lib/modules/Google/handlers/Cron.php
@@ -5,5 +5,11 @@ class Google_Cron_Handler
     public static function runSchedule()
     {
         echo "Running Google Sync...\n";
+
+        $allUsers = getAllUserName();
+
+        foreach ($allUsers as $user) {
+            var_dump($user);
+        }
     }
 }
