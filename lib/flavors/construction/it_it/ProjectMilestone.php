c15beafa2f394f37b5646e2dfe25ae19ca082ac0
francesco <info.francescobianco@gmail.com>
2026-03-19 15:50:59 +0100

    release

---
+++ lib/flavors/construction/it_it/ProjectMilestone.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = [
+    'SINGLE_ProjectMilestone'              => 'Lavorazione',
+    'LBL_ADD_RECORD'                       => 'Aggiungi Lavorazione',
+    'LBL_RECORDS_LIST'                     => 'Lista Lavorazioni',
+    'LBL_PROJECTS_LIST'                    => 'Lista Cantieri',
+    'LBL_TASKS_LIST'                       => 'Lista Attivit&agrave;',
+    'LBL_MILESTONES_LIST'                  => 'Lista Lavorazioni',
+    'LBL_PROJECT_MILESTONE_INFORMATION'    => 'Dettagli Lavorazione',
+    'Project Milestone Name'               => 'Nome Lavorazione',
+    'Milestone Date'                       => 'Data Lavorazione',
+    'Project Milestone No'                 => 'Numero Lavorazione',
+    'Type'                                 => 'Tipo Lavorazione',
+    //picklist values
+    'administrative'               => 'Amministrativo',
+    'operative'                    => 'Operativo',
+    'other'                        => 'Altro',
+    'Related to'                   => 'Relativi a',
+];
